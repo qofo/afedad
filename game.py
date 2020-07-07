dafb6d0f554e97830fe4fd702d6da71680311f0a
qofo <junseolee02@gmail.com>
2020-07-08 01:27:53 +0900

    WTF

--- game.py
+++ game.py
@@ -1,8 +1,19 @@
 import tcod as libtcod
 import sys
+import time
+import threading
 
 
-
+class Thread(threading.Thread):#########안 
+    def run(self):
+        if screen.textline != []:
+            print(self.is_alive())
+            time.sleep(2)
+            screen.remove_text()
+            return 0
+        else:
+            print('no')
+       
 class Solid:
     pass
 
@@ -24,10 +35,17 @@ class Screen:
     def __init__(self, width, height):
         self.width = width
         self.height = height
+        self.textline = []
+
+        #스테이지 설정
         self.room = [[Path() for i in range(self.width)] for j in range(self.height)]
-    def draw(self, con, player):
-        libtcod.console_set_default_foreground(con, libtcod.white)              #모름
-        if player.x < 10: a = 0                                                 #player 기준 시야 범위 설정 
+
+        
+    def draw(self, player):
+        libtcod.console_set_default_foreground(con, libtcod.white)
+        
+        #player 기준 시야 범위 설정
+        if player.x < 10: a = 0                                                  
         else: a = player.x - 10
         if player.x > self.width - 11: b = self.width
         else: b = player.x + 10
@@ -35,19 +53,39 @@ class Screen:
         else: c = player.y - 10
         if player.y > self.height - 11: d = self.height
         else: d = player.y + 10
-        for i in range(c, d):
-            for j in range(a, b):
-                libtcod.console_put_char(con, j, i, self.room[i][j].char, libtcod.BKGND_NONE)#그리기
-        libtcod.console_blit(con, 0, 0, self.width, self.height, 0, 0, 0)       #모름
-        libtcod.console_flush()                                                 #This is the part that presents everything on the screen. Pretty straightforward.모름
-        for i in range(c, d):
-            for j in range(a, b):
-                libtcod.console_put_char(con, j, i, ' ', libtcod.BKGND_NONE)
-        libtcod.console_put_char(con, player.x, player.y, player.ex_object.char, libtcod.BKGND_NONE)#원래 있던 자리 지움
 
+        #그리기
+        for i in range(c, d):
+            for j in range(a, b):
+                libtcod.console_put_char(con, j, i+5, self.room[i][j].char, libtcod.BKGND_NONE)#그리기
+                
+        libtcod.console_blit(con, 0, 0, self.width, self.height+10, 0, 0, 0)
+        libtcod.console_flush()                                                 #This is the part that presents everything on the screen. Pretty straightforward
+
+        #원래 있던 자리 지우기
+        for i in range(c, d):
+            for j in range(a, b):
+                libtcod.console_put_char(con, j, i+5, ' ', libtcod.BKGND_NONE)
+        libtcod.console_put_char(con, player.x, player.y+5, player.ex_object.char, libtcod.BKGND_NONE)#원래 있던 자리 지움
+
+        
+    def draw_text(self, text):
+        self.textline.append(text)
+        print(self.textline)
+        for i, s in enumerate(text):
+            libtcod.console_put_char(con, i, len(self.textline)-1, s, libtcod.BKGND_NONE)
+        #libtcod.console_blit(con, 0, 0, screen.width, screen.height+10, 0, 0, 0)
+        #libtcod.console_flush()
+    def remove_text(self):
+        if self.textline != []:
+            for i in range(30):
+                libtcod.console_put_char(con, i, len(self.textline)-1, ' ', libtcod.BKGND_NONE)
+            self.textline.pop()
+            print(self.textline)
+        
 
 class Entity(Solid):
-    def __init__(self, x, y, char):
+    def __init__(self, x, y, char): 
         self.char = char
         
         self.x = x
@@ -56,13 +94,16 @@ class Entity(Solid):
         screen.room[y][x] = self
     hp = 10
 
-    def move(self, dx, dy):####오류가 있을 수 있음.
+
+    def move(self, dx, dy):
         
         if self.x + dx > screen.width-1 or self.x + dx < 0:
             dx = 0
         if self.y + dy > screen.height-1 or self.y + dy < 0:
             dy = 0
-        if not isinstance(screen.room[self.y+dy][self.x+dx],Solid):
+        if isinstance(screen.room[self.y+dy][self.x+dx],Solid):
+            screen.draw_text("You can't move there")
+        else:
             screen.room[self.y][self.x] = self.ex_object
             self.x += dx
             self.y += dy
@@ -72,10 +113,12 @@ class Entity(Solid):
 class Enemy(Entity):
     char = '+'
 
+
+
+
 def main():
 
-
-    ##GUI설정
+    #GUI설정
     global screen
     screen = Screen(80, 50)
     
@@ -83,29 +126,31 @@ def main():
     for i in range(40):
         Enemy(i, i, '+')
     
-    
-    #libtcod.console_set_custom_font("arial10x10.png", libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD) #폰트를 지정한 png파일로 바꿈
-    libtcod.console_init_root(screen.width, screen.height, "qqqqqqqqqq", False) #창만들기(창 가로, 높이, "창의 이름", 전체화면 여부)
-    ##
+    libtcod.console_set_custom_font("arial10x10.png", libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD) #폰트를 지정한 png파일로 바꾸기
+    libtcod.console_init_root(screen.width, screen.height+10, "qqqqqqqqqq", False) #창만들기(창 가로,창 높이, 창의 이름, 전체화면 여부)
 
-    ##객체 설정
+    #객체 설정
     player = Entity(45, 35, '@')
-    ##
 
-    con = libtcod.console_new(screen.width, screen.height)                      #새로운 콘솔창 만들기
+    #콘솔창 설정
+    global con
+    con = libtcod.console_new(screen.width, screen.height+10)                   #새로운 콘솔창 만들기
+    key = libtcod.Key()                                                         #키보드 입력(상하좌우, 엔터 등)
     
-    key = libtcod.Key()                                                         #키보드 입력
     mouse = libtcod.Mouse()                                                     #마우스 입력
 
-    ##반복문
+    
+    #무한 반복문
     while not libtcod.console_is_window_closed():                               #창이 안 닫혔을 때
 
+        key_char = chr(key.c)                                                   #키보드 입력(문자)
 
-        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)        #뭔지 모름
-        
-        screen.draw(con, player)                                                #화면에 그리기
+        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
 
-        ##키 관련
+        screen.draw(player)                                                     #화면에 그리기
+        #키 실행
+        if key.vk and screen.textline != []:
+            screen.remove_text()
         if key.vk == libtcod.KEY_ESCAPE:
             return True                                                         #게임 종료
         elif key.vk == libtcod.KEY_ENTER and key.lalt:
@@ -118,8 +163,9 @@ def main():
             player.move(0,-1)
         elif key.vk == libtcod.KEY_DOWN:
             player.move(0,1)
-        ##
-    ##
+        elif key_char == 'q':
+            screen.draw_text("asdasd")
+
 if __name__ == "__main__":                                                      #프로그램이 직접실행되었을 때
     main()
 
